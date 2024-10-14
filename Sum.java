b74882b8c1633ebfac8d9d8029ee9fa3cef40996
Sahana8402 <85979149+Sahana8402@users.noreply.github.com>
2024-10-14 17:34:57 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,9 @@
+publuc class Sum{
+  public static void main (String[] args){
+    int a= 10;
+    int b=20;
+    int c=a+b;
+    System.out.print("Sum of " + a + " and" + b + " is " + c);
+  }
+  
+}
