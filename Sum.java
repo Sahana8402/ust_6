16230920034b26cb2346d9f9912765fdb2671168
Sahana8402 <85979149+Sahana8402@users.noreply.github.com>
2024-10-14 17:42:05 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,9 +1,9 @@
 public class Sum{
-  public static void main (String[] args){
-    int a= 10;
+  public static void main(String[] args){
+    int a=10;
     int b=20;
     int c=a+b;
-    System.out.print("Sum of " + a + " and" + b + " is " + c);
+    System.out.println("10+20="+c);
   }
   
 }
